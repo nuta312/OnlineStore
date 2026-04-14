549c13ff44fde56f0bf1540a10df4c38f3a3459b
AtaiSDET <salamatovatai@gmail.com>
2026-04-14 19:07:26 +0400

    added some more tests to GUI

--- src/main/java/kg/benext/gui/pages/RegisterPage.java
+++ src/main/java/kg/benext/gui/pages/RegisterPage.java
@@ -2,19 +2,30 @@ package kg.benext.gui.pages;
 
 import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Condition.attribute;
 import static com.codeborne.selenide.Condition.clickable;
+import static com.codeborne.selenide.Condition.cssClass;
+import static com.codeborne.selenide.Condition.cssValue;
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class RegisterPage extends BasePage<RegisterPage>{
+    private final String PLACEHOLDER = "placeholder";
     private final SelenideElement fullName = $("input[name='fullName']");
     private final SelenideElement email = $("input[name='email']");
     private final SelenideElement password = $("input[name='password']");
     private final SelenideElement confirmPassword = $("input[name='confirmPassword']");
     private final SelenideElement terms = $("#terms");
     private final SelenideElement createAccount = $x("//button[text()='Create Account']");
+    private final SelenideElement emailInUseNotification = $x("//div[text()='Email is already in use']");
+    private final SelenideElement fullNameErrorMessage = $x("//p[text()='Name must be at least 2 characters']");
+    private final SelenideElement emailErrorMessage = $x("//p[text()='Please enter a valid email address']");
+    private final SelenideElement passwordErrorMessage = $x("//p[text()='Password must be at least 6 characters']");
+    private final SelenideElement confirmPasswordErrorMessage = $x("//p[contains(text(), \"Passwords don't match\")]");
+    private final SelenideElement termsErrorMessage = $x("//p[text()='You must accept the terms and conditions']");
+    private final SelenideElement google = $x("//button[text()='Google']");
 
     @Override
     public RegisterPage waitForPageToBeLoaded() {
@@ -47,10 +58,68 @@ public class RegisterPage extends BasePage<RegisterPage>{
         return this;
     }
 
-    public HomePage clickToCreateAccountBtn() {
+    public HomePage clickCreateAccountSuccess() {
         createAccount.shouldBe(visible).click();
         return new HomePage();
     }
 
+    public RegisterPage clickCreateAccountExpectError() {
+        createAccount.shouldBe(visible).click();
+        return this;
+    }
 
+    public RegisterPage verifyEmailInUseMessage() {
+        emailInUseNotification.shouldBe(visible).shouldHave(cssClass("text-red-500"));
+        return this;
+    }
+
+    public RegisterPage verifyFullNameIsRequiredMessage() {
+        fullNameErrorMessage.shouldBe(visible).shouldHave(cssClass("text-red-500"));
+        return this;
+    }
+
+    public RegisterPage verifyEmailIsRequiredMessage() {
+        emailErrorMessage.shouldBe(visible).shouldHave(cssClass("text-red-500"));
+        return this;
+    }
+
+    public RegisterPage verifyPasswordIsRequiredMessage() {
+        passwordErrorMessage.shouldBe(visible).shouldHave(cssClass("text-red-500"));
+        return this;
+    }
+
+    public RegisterPage verifyConfirmPasswordIsRequiredMessage() {
+        confirmPasswordErrorMessage.shouldBe(visible).shouldHave(cssClass("text-red-500"));
+        return this;
+    }
+
+    public RegisterPage verifyTermsIsRequiredMessage() {
+        termsErrorMessage.shouldBe(visible).shouldHave(cssClass("text-red-500"));
+        return this;
+    }
+
+    public RegisterPage clickToRegisterByGoogle() {
+        google.shouldBe(visible).click();
+        return this;
+    }
+
+    public RegisterPage verifyFullNamePlaceholder() {
+        fullName.shouldHave(attribute(PLACEHOLDER, "John Doe"));;
+        return this;
+    }
+
+    public RegisterPage verifyEmailPlaceholder() {
+        email.shouldHave(attribute(PLACEHOLDER, "name@example.com"));;
+        return this;
+    }
+
+    public RegisterPage verifyPasswordPlaceholder() {
+        password.shouldHave(attribute(PLACEHOLDER, "Create a password"));;
+        return this;
+    }
+
+    public RegisterPage verifyConfirmPasswordPlaceholder() {
+        confirmPassword.shouldHave(attribute(PLACEHOLDER, "Confirm your password"));;
+        return this;
+    }
 }
--- src/test/java/gui/signUp/SignUpTest.java
+++ src/test/java/gui/signUp/SignUpTest.java
@@ -4,8 +4,13 @@ import com.codeborne.selenide.Selenide;
 import gui.BaseGUI;
 import kg.benext.common.utils.file.ConfigurationManager;
 import kg.benext.gui.pages.HomePage;
+import kg.benext.gui.pages.RegisterPage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.switchTo;
+import static com.codeborne.selenide.Selenide.webdriver;
+import static com.codeborne.selenide.WebDriverConditions.urlContaining;
 
 public class SignUpTest extends BaseGUI {
 
@@ -16,18 +21,18 @@ public class SignUpTest extends BaseGUI {
             new HomePage()
                 .clickToLogin().waitForPageToBeLoaded()
                 .clickToRegisterBtn().waitForPageToBeLoaded()
-                .inputFullName("John Doe")
-                .inputEmail("JohnDoe@exapmle.com")
+                .inputFullName("Jane Doe")
+                .inputEmail("JaneDoe@exapmle.com")
                 .inputPassword("123456")
                 .inputConfirmPassword("123456")
                 .acceptTerms()
-                .clickToCreateAccountBtn().waitForPageToBeLoaded()
+                .clickCreateAccountSuccess().waitForPageToBeLoaded()
                 .verifyProfileImageIsDisplayed();
     }
     // test is displayed Email is already in use with red font color
     @Test
     @DisplayName("Create user with same Credentials")
-    void createUserTest() {
+    void createUserWithSameCredentialTest() {
         Selenide.open(ConfigurationManager.getBaseConfig().baseUrl());
         new HomePage()
                 .clickToLogin().waitForPageToBeLoaded()
@@ -37,7 +42,157 @@ public class SignUpTest extends BaseGUI {
                 .inputPassword("123456")
                 .inputConfirmPassword("123456")
                 .acceptTerms()
-                .clickToCreateAccountBtn().waitForPageToBeLoaded()
-                .verifyProfileImageIsDisplayed();
+                .clickCreateAccountExpectError()
+                .verifyEmailInUseMessage();
+    }
+
+    @Test
+    @DisplayName("Create user with empty full name")
+    void createUserWithEmptyFullNameTest() {
+        Selenide.open(ConfigurationManager.getBaseConfig().baseUrl());
+        new HomePage()
+                .clickToLogin().waitForPageToBeLoaded()
+                .clickToRegisterBtn().waitForPageToBeLoaded()
+                .inputEmail("JohnDoe@exapmle.com")
+                .inputPassword("123456")
+                .inputConfirmPassword("123456")
+                .acceptTerms()
+                .clickCreateAccountExpectError()
+                .verifyFullNameIsRequiredMessage();
+    }
+
+    @Test
+    @DisplayName("Create user with one letter full name")
+    void createUserWithNotValidFullNameTest() {
+        Selenide.open(ConfigurationManager.getBaseConfig().baseUrl());
+        new HomePage()
+                .clickToLogin().waitForPageToBeLoaded()
+                .clickToRegisterBtn().waitForPageToBeLoaded()
+                .inputFullName("J")
+                .inputEmail("JohnDoe@exapmle.com")
+                .inputPassword("123456")
+                .inputConfirmPassword("123456")
+                .acceptTerms()
+                .clickCreateAccountExpectError()
+                .verifyFullNameIsRequiredMessage();
+    }
+
+    @Test
+    @DisplayName("Create user with empty email")
+    void createUserWithEmptyEmailTest() {
+        Selenide.open(ConfigurationManager.getBaseConfig().baseUrl());
+        new HomePage()
+                .clickToLogin().waitForPageToBeLoaded()
+                .clickToRegisterBtn().waitForPageToBeLoaded()
+                .inputFullName("John Doe")
+                .inputPassword("123456")
+                .inputConfirmPassword("123456")
+                .acceptTerms()
+                .clickCreateAccountExpectError()
+                .verifyEmailIsRequiredMessage();
+    }
+
+    @Test
+    @DisplayName("Create user with not valid email")
+    void createUserWithNotValidEmailTest() {
+        Selenide.open(ConfigurationManager.getBaseConfig().baseUrl());
+        new HomePage()
+                .clickToLogin().waitForPageToBeLoaded()
+                .clickToRegisterBtn().waitForPageToBeLoaded()
+                .inputFullName("John Doe")
+                .inputEmail("JohnDoe@exapmle")
+                .inputPassword("123456")
+                .inputConfirmPassword("123456")
+                .acceptTerms()
+                .clickCreateAccountExpectError()
+                .verifyEmailIsRequiredMessage();
+    }
+
+    @Test
+    @DisplayName("Create user with empty password")
+    void createUserWithEmptyPasswordTest() {
+        Selenide.open(ConfigurationManager.getBaseConfig().baseUrl());
+        new HomePage()
+                .clickToLogin().waitForPageToBeLoaded()
+                .clickToRegisterBtn().waitForPageToBeLoaded()
+                .inputFullName("John Doe")
+                .inputEmail("JohnDoe@exapmle.com")
+                .inputConfirmPassword("123456")
+                .acceptTerms()
+                .clickCreateAccountExpectError()
+                .verifyPasswordIsRequiredMessage();
+    }
+
+    @Test
+    @DisplayName("Create user with less characters password")
+    void createUserWithLessCharactersPasswordTest() {
+        Selenide.open(ConfigurationManager.getBaseConfig().baseUrl());
+        new HomePage()
+                .clickToLogin().waitForPageToBeLoaded()
+                .clickToRegisterBtn().waitForPageToBeLoaded()
+                .inputFullName("John Doe")
+                .inputEmail("JohnDoe@exapmle.com")
+                .inputPassword("12345")
+                .inputConfirmPassword("12345")
+                .acceptTerms()
+                .clickCreateAccountExpectError()
+                .verifyPasswordIsRequiredMessage();
+    }
+
+    @Test
+    @DisplayName("Create user with empty confirm password")
+    void createUserWithEmptyConfirmPasswordTest() {
+        Selenide.open(ConfigurationManager.getBaseConfig().baseUrl());
+        new HomePage()
+                .clickToLogin().waitForPageToBeLoaded()
+                .clickToRegisterBtn().waitForPageToBeLoaded()
+                .inputFullName("John Doe")
+                .inputEmail("JohnDoe@exapmle.com")
+                .inputPassword("123456")
+                .acceptTerms()
+                .clickCreateAccountExpectError()
+                .verifyConfirmPasswordIsRequiredMessage();
+    }
+
+    @Test
+    @DisplayName("Create user with empty confirm password")
+    void createUserWithNoTermsAcceptTest() {
+        Selenide.open(ConfigurationManager.getBaseConfig().baseUrl());
+        new HomePage()
+                .clickToLogin().waitForPageToBeLoaded()
+                .clickToRegisterBtn().waitForPageToBeLoaded()
+                .inputFullName("John Doe")
+                .inputEmail("JohnDoe@exapmle.com")
+                .inputPassword("123456")
+                .inputConfirmPassword("123456")
+                .clickCreateAccountExpectError()
+                .verifyTermsIsRequiredMessage();
+    }
+
+    //Написать Тест на проверку ввода имени только из ЦИФР
+
+    @Test
+    @DisplayName("Register by Google Auth and Check if new window is accounts.google.com")
+    void createUserByGoogle() {
+        Selenide.open(ConfigurationManager.getBaseConfig().baseUrl());
+        new HomePage()
+                .clickToLogin().waitForPageToBeLoaded()
+                .clickToRegisterBtn().waitForPageToBeLoaded()
+                .clickToRegisterByGoogle();
+                switchTo().window(1);
+                webdriver().shouldHave(urlContaining("accounts.google.com"));
+    }
+
+    @Test
+    @DisplayName("Verify all placeholders")
+    void verifyPlaceholdersTest() {
+        Selenide.open(ConfigurationManager.getBaseConfig().baseUrl());
+        new HomePage()
+                .clickToLogin().waitForPageToBeLoaded()
+                .clickToRegisterBtn().waitForPageToBeLoaded()
+                .verifyFullNamePlaceholder()
+                .verifyEmailPlaceholder()
+                .verifyPasswordPlaceholder()
+                .verifyConfirmPasswordPlaceholder();
     }
 }
